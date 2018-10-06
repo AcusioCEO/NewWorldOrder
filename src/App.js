5143d899a537381a4212431f741d7cc144907f96
AcusioCEO <AcusioCEO@Gmail.com>
2018-10-06 11:12:26 -0700

    Just setting up.

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>New World Order Records ✪</h1>
+        <h1>New World Order Records ✪</h1>
+        <h1>New World Order Records ✪</h1>
+        <h1>New World Order Records ✪</h1>
+        <h1>New World Order Records ✪</h1>
+        <h1>New World Order Records ✪</h1>
+        <h1>New World Order Records ✪</h1>
+
+        <h1>New World Order Records ✪</h1>
+        <h1>New World Order Records ✪</h1>
+        <h1>New World Order Records ✪</h1>
+        <h1>New World Order Records ✪</h1>
+
+        <h1>New World Order Records ✪</h1>
+        <h1>New World Order Records ✪</h1>
+
+        <h1>New World Order Records ✪</h1>
+
+        <h1>New World Order Records ✪</h1>
+        <h1>New World Order Records ✪</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
